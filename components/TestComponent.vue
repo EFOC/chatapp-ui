0f4cbdc4db5e3af23d8c830efcdfbec31f5a9711
Eduardo Osorio <osorio-cruz@hotmail.com>
2022-03-26 15:31:05 -0400

    project init

---
+++ components/TestComponent.vue
@@ -0,0 +1,24 @@
+<!-- Please remove this file from your project -->
+<template>
+
+<div>
+    <p>Test Component 1234</p>
+    <p> {{ header }} </p>
+</div>
+
+</template>
+
+<script>
+export default {
+
+    props:{
+        header: String
+    },
+    
+    data() {
+        return {
+            // header: this.header
+        }
+    }
+}
+</script>
---
+++ components/Tutorial.vue
@@ -0,0 +1,51 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    <test-component :header="title"></test-component>
+    <button @click="changeTitle">Test</button>
+  </div>
+</template>
+
+<script>
+import TestComponent from './TestComponent.vue'
+export default {
+  components: { TestComponent },
+  data() {
+    return {
+      name: "test",
+      counter: this.$store.state.counter,
+      title: "The Title"
+    }
+  },
+
+  methods: {
+    test() {
+      console.log("created", this.counter)
+      this.$store.commit('increment')
+      console.log("counter", this.counter)
+      console.log("state counter", this.$store.state.counter)
+      this.counter = this.$store.state.counter
+      
+    },
+    changeTitle() {
+      this.title = "new one"
+    }
+  },
+
+  created: function () {
+    this.test()
+  },
+  watch: {
+    counter: function() {
+      console.log("changing")
+      return this.counter
+    }
+  },
+
+  computed: {
+    counterTest() {
+      return this.$store.state.counter
+    }
+  }
+}
+</script>
